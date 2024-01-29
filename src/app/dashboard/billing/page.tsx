5bf0987cc45bd918a6b9515cd65694248af46c81
Mangesh Ahirrao <mahirrao925@gmail.com>
2024-01-29 00:03:46 -0600

    page change

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,24 +1,7 @@
 import BillingForm from "@/components/BillingForm";
-import Dashboard from "@/components/Dashboard";
-import { db } from "@/db";
 import { getUserSubscriptionPlan } from "@/lib/stripe";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
 
 const Page = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = getUser();
-
-  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
-
-  const dbUser = await db.user.findFirst({
-    where: {
-      id: user.id,
-    },
-  });
-
-  if (!dbUser) redirect("/auth-callback?origin=dashboard");
-
   const subscriptionPlan = await getUserSubscriptionPlan();
 
   return <BillingForm subscriptionPlan={subscriptionPlan} />;
