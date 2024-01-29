622d6863e23a50c44bd28cbb50f8562481dd0dc4
Mangesh Ahirrao <mahirrao925@gmail.com>
2024-01-28 20:55:46 -0600

    initial commit

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,70 @@
+import { db } from "@/db";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { pinecone } from "@/lib/pinecone";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
+      const { getUser } = getKindeServerSession();
+      const user = getUser();
+      if (!user || !user.id) throw new Error("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: file.url,
+          uploadStatus: "PROCESSING",
+        },
+      });
+
+      try {
+        const response = await fetch(file.url);
+        const blob = await response.blob();
+        const loader = new PDFLoader(blob);
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmt = pageLevelDocs.length;
+        const pineconeIndex = pinecone.Index("docuchatter");
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (err) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
